680116cdebbf381fcdac4fc75cf1f45ffa4bd59b
nigeshj <47964143+nigeshj@users.noreply.github.com>
2019-03-06 14:28:47 +0530

    web api example files added

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiExample.Controllers
+{
+    public class CourseController : ApiController
+    {
+        public List<Course> GetCourses()
+        {
+            StudentEntities ss = new StudentEntities();
+            
+                return ss.Courses.ToList();
+            
+        }
+
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiExample.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public List<Student> GetStudents()
+        {
+            using (StudentEntities ss = new StudentEntities())
+            {
+                return ss.Students.ToList();
+            }
+        }
+    }
+}
